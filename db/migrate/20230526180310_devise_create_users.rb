7ff250289f7794e9d3da79def008465ebffe498d
Odell Purifoy <opurifoy@springhealth.com>
2023-05-26 14:16:24 -0400

    Migrated db

--- db/migrate/20230526180310_devise_create_users.rb
+++ db/migrate/20230526180310_devise_create_users.rb
@@ -4,6 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.date   :date_of_birth,      null: false
+      t.string :username,           null: false
+      t.string :phone_number
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
