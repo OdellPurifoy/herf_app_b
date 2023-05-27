bebdd6482e547b051e6079b99321ef72f2b7eb85
Odell Purifoy <opurifoy@springhealth.com>
2023-05-26 23:07:23 -0400

    Converted to uuid

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_180310) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_27_025948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.bigint "integer_id"
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.date "date_of_birth", null: false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,20 +12,20 @@ RSpec.describe User, type: :model do
     it { should have_db_column(:username).of_type(:string) }
   end
 
-  describe 'Validations' do
-    it { should validate_presence_of(:first_name) }
-    it { should validate_presence_of(:last_name) }
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:date_of_birth) }
-    it { should validate_presence_of(:username) }
-  end
-
   describe 'Custom validations' do
-    context 'when user is 18 years or older' do
+    context 'when a user is under 18 years of age' do
       let(:user) { FactoryBot.create(:user, date_of_birth: (Time.zone.now - 12.years).to_date) }
 
       it 'raises an error' do
         expect { user }.to raise_error
+      end
+    end
+
+    context 'when a user if older than 18 year of age' do
+      let(:user) { FactoryBot.create(:user, date_of_birth: (Time.zone.now - 19.years).to_date) }
+
+      it 'does not raise an error' do
+        expect { user }.to_not raise_error
       end
     end
   end
