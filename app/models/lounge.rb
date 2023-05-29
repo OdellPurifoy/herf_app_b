177843297400ed42bc3e1511aaca4349b7c9a401
Odell Purifoy <opurifoy@springhealth.com>
2023-05-28 21:15:44 -0400

    Updated lounge factory and added model validation

--- app/models/lounge.rb
+++ app/models/lounge.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Lounge < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :name, :phone_number, :description
+  validates :description, length: { maximum: 500, too_long: '%<count>s characters is the maximum allowed' }
 end
--- spec/factories/lounges.rb
+++ spec/factories/lounges.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :lounge do
-    name { "MyString" }
-    phone_number { "MyString" }
-    email { "MyString" }
-    description { "MyText" }
+    name { "#{Faker::Name.first_name}s_Lounge" }
+    phone_number { Faker::PhoneNumber.cell_phone } #=> "(186)285-7925"
+    email { "#{name}_#{phone_number}@example.com" }
+    description { Faker::Lorem.sentence(word_count: 50) }
     alcohol_served { false }
     food_served { false }
     outside_alcohol_allowed { false }
     outside_cigars_allowed { false }
-    user { nil }
+    user
   end
 end
