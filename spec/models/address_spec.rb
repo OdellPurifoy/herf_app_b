584859d450d23bf986d49edeb8ec59a12f678288
Odell Purifoy <opurifoy@springhealth.com>
2023-06-03 20:11:53 -0400

    Added lounge and address validations

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe Address, type: :model do
       expect { address }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
+
+  describe 'Zipcode validation' do
+    let(:address) { FactoryBot.create(:address, zipcode: "3463fsgfsfgs") }
+
+    it "is not a valid zipcode" do
+      expect{address}.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    context "when valid 5 digit zipcode is used" do
+      let(:address) { FactoryBot.create(:address, zipcode: "11211") }
+
+      it "does not raise an error" do
+        expect{address}.to_not raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context "when valid 9 digit zip code is used" do
+      let(:address) { FactoryBot.create(:address, zipcode: "12345-1234") }
+
+      it "does not raise an error" do
+        expect{address}.to_not raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
--- spec/models/lounge_spec.rb
+++ spec/models/lounge_spec.rb
@@ -44,4 +44,20 @@ RSpec.describe Lounge, type: :model do
       expect { lounge }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
+
+  describe 'Email REGEX validation' do
+    let(:lounge) { FactoryBot.create(:lounge, email: 'invalid_emailexample.com') }
+
+    it "raises an Email invalid error" do
+      expect{lounge}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Email Email invalid")
+    end
+
+    context "when valid email is used" do
+      let(:lounge) { FactoryBot.create(:lounge, email: 'valid_email@example.com') }
+
+      it "does not raise an error" do
+        expect{lounge}.to_not raise_error
+      end
+    end
+  end
 end
