762181a2b662ef5d4404d33a36ac578113d400a8
Odell Purifoy <opurifoy@springhealth.com>
2023-06-14 19:20:06 -0400

    Updated events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,31 +1,29 @@
+# frozen_string_literal: true
+
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :set_event, only: %i[show edit update destroy]
+  before_action :set_lounge, only: %i[index new create]
+  before_action :authenticate_user, only: %i[new edit create update destroy]
 
-  # GET /events or /events.json
   def index
-    @events = Event.all
+    @events = @lounge.events.order(:created_at).page(params[:page])
   end
 
-  # GET /events/1 or /events/1.json
-  def show
-  end
+  def show; end
 
-  # GET /events/new
   def new
-    @event = Event.new
+    @event = @lounge.events.build
   end
 
-  # GET /events/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /events or /events.json
   def create
-    @event = Event.new(event_params)
+    @event = @lounge.events.build(event_params)
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to event_url(@event), notice: "Event was successfully created." }
+        format.turbo_stream { redirect_to event_path(@event) }
+        format.html { redirect_to event_url(@event), notice: 'Event was successfully created.' }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,11 +32,11 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1 or /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to event_url(@event), notice: "Event was successfully updated." }
+        format.turbo_stream { redirect_to [@lounge, @event] }
+        format.html { redirect_to event_url(@event), notice: 'Event was successfully updated.' }
         format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,24 +45,25 @@ class EventsController < ApplicationController
     end
   end
 
-  # DELETE /events/1 or /events/1.json
   def destroy
     @event.destroy
 
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to root_path, status: :see_other
+    flash[:notice] = 'Event successfully deleted.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def event_params
-      params.require(:event).permit(:event_name, :event_type, :event_url, :zoom_code, :rsvp_needed, :maximum_capacity, :start_time, :end_time, :members_only, :lounge_id)
-    end
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def set_lounge
+    @lounge = Lounge.find(params[:lounge_id])
+  end
+
+  def event_params
+    params.require(:event).permit(:event_name, :event_type, :event_url, :zoom_code, :rsvp_needed, :maximum_capacity,
+                                  :start_time, :end_time, :members_only, :lounge_id)
+  end
 end
