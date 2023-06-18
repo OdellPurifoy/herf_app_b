4faf6d0c4dea62fd54327f79aeaaa3d24bd25c85
Odell Purifoy <opurifoy@springhealth.com>
2023-06-17 23:59:53 -0400

    Added rsvps and specs

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -4,5 +4,5 @@ class Rsvp < ApplicationRecord
   belongs_to :event
 
   validates_presence_of :first_name, :last_name
-  validates :number_of_guests, numericality: { only_integer: true }
+  validates :number_of_guests, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- spec/models/rsvp_spec.rb
+++ spec/models/rsvp_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Rsvp, type: :model do
   it { should have_db_column(:phone_number).of_type(:string) }
   it { should have_db_column(:number_of_guests).of_type(:integer) }
   it { should have_db_index([:event_id]) }
+
+  describe 'Number of guests validation' do
+    let(:rsvp) { FactoryBot.create(:rsvp, number_of_guests: 2) }
+
+    it 'does not raise an error' do
+      expect{rsvp}.to_not raise_error
+    end
+
+    context 'when the number_of_guests is less than zero' do
+      let(:rsvp) { FactoryBot.create(:rsvp, number_of_guests: -2) }
+
+      it 'raises a validation error' do
+        expect{rsvp}.to raise_error
+      end
+    end
+  end
 end
