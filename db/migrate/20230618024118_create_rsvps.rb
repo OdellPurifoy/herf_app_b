a48654887c1bc26d486979e015bb6d4df6efca60
Odell Purifoy <opurifoy@springhealth.com>
2023-06-17 22:50:42 -0400

    Ran migration

--- db/migrate/20230618024118_create_rsvps.rb
+++ db/migrate/20230618024118_create_rsvps.rb
@@ -1,8 +1,8 @@
 class CreateRsvps < ActiveRecord::Migration[7.0]
   def change
     create_table :rsvps, id: :uuid do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
       t.string :phone_number
       t.string :email
       t.integer :number_of_guests
