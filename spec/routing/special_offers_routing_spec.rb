07d567ef6fde15b149461ca5edb5c354ab56bda5
Odell Purifoy <opurifoy@springhealth.com>
2023-06-23 19:07:51 -0400

    Updated form and routing specs

--- spec/routing/special_offers_routing_spec.rb
+++ spec/routing/special_offers_routing_spec.rb
@@ -1,38 +1,51 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe SpecialOffersController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/special_offers").to route_to("special_offers#index")
+  describe 'routing' do
+    let(:lounge) { FactoryBot.create(:lounge) }
+    let(:special_offer) { FactoryBot.create(:special_offer) }
+    let(:user) { FactoryBot.create(:user) }
+
+    it 'routes to #index' do
+      expect(get: "/lounges/#{lounge.id}/special_offers").to route_to(controller: 'special_offers',
+                                                                      action: 'index', lounge_id: lounge.id.to_s)
     end
 
-    it "routes to #new" do
-      expect(get: "/special_offers/new").to route_to("special_offers#new")
+    it 'routes to #new' do
+      expect(get: "/lounges/#{lounge.id}/special_offers/new").to route_to(controller: 'special_offers',
+                                                                          action: 'new', lounge_id: lounge.id.to_s)
     end
 
-    it "routes to #show" do
-      expect(get: "/special_offers/1").to route_to("special_offers#show", id: "1")
+    it 'routes to #show' do
+      expect(get: "/special_offers/#{special_offer.id}").to route_to(controller: 'special_offers',
+                                                                     action: 'show', id: special_offer.id.to_s)
     end
 
-    it "routes to #edit" do
-      expect(get: "/special_offers/1/edit").to route_to("special_offers#edit", id: "1")
+    it 'routes to #edit' do
+      expect(get: "/special_offers/#{special_offer.id}/edit").to route_to(controller: 'special_offers',
+                                                                          action: 'edit', id: special_offer.id.to_s)
     end
 
-
-    it "routes to #create" do
-      expect(post: "/special_offers").to route_to("special_offers#create")
+    it 'routes to #create' do
+      expect(post: "/lounges/#{lounge.id}/special_offers").to route_to(controller: 'special_offers',
+                                                                       action: 'create', lounge_id: lounge.id.to_s)
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/special_offers/1").to route_to("special_offers#update", id: "1")
+    it 'routes to #update via PUT' do
+      expect(put: "/special_offers/#{special_offer.id}").to route_to(controller: 'special_offers',
+                                                                     action: 'update', id: special_offer.id.to_s)
     end
 
-    it "routes to #update via PATCH" do
-      expect(patch: "/special_offers/1").to route_to("special_offers#update", id: "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: "/special_offers/#{special_offer.id}").to route_to(controller: 'special_offers',
+                                                                       action: 'update', id: special_offer.id.to_s)
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/special_offers/1").to route_to("special_offers#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: "/special_offers/#{special_offer.id}").to route_to(controller: 'special_offers',
+                                                                        action: 'destroy', id: special_offer.id.to_s)
     end
   end
 end
