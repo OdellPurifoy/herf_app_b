0a577b844a411de9011862970f943908d102a058
Odell Purifoy <opurifoy@springhealth.com>
2023-06-29 16:48:40 -0400

    Updated event creation form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :events do
+    resources :event_flyers, only: [:destroy]
     resources :rsvps, shallow: true
   end
   
---
+++ spec/requests/event_flyers_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "EventFlyers", type: :request do
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/event_flyers/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
