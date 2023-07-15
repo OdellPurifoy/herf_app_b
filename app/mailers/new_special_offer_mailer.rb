2ea882b874e2f52a438e5aae7b0852c5b8163e15
Odell Purifoy <opurifoy@springhealth.com>
2023-07-14 22:32:19 -0400

    Added mailer for new special offer

---
+++ app/mailers/new_special_offer_mailer.rb
@@ -0,0 +1,12 @@
+class NewSpecialOfferMailer < ApplicationMailer
+  def notify
+    @special_offer = params[:special_offer]
+    @membership = params[:membership]
+
+    mail(
+      from: 'herf@gmail.com',
+      to: @membership.email,
+      subject: 'New Special Offer!'
+    )  
+  end
+end
---
+++ spec/mailers/previews/new_special_offer_preview.rb
@@ -0,0 +1,18 @@
+# Preview all emails at http://localhost:3000/rails/mailers/new_special_offer
+class NewSpecialOfferPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/new_special_offer/notify
+  def notify
+    NewSpecialOfferMailer.with(special_offer: special_offer, membership: membership).notify
+  end
+
+  private
+
+  def special_offer
+    SpecialOffer.last
+  end
+
+  def membership
+    Membership.last
+  end
+end
